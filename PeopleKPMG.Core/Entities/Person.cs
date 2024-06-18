e6ffd93b66a77dbdf9adb01eb978708d1ab03854
Vitor Lopes <vitor_slopes@hotmail.com>
2024-06-18 16:39:50 -0300

    adiciona entidades e interfaces do Core

---
+++ PeopleKPMG.Core/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleKPMG.Core.Entities
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Dependent>? Dependents { get; set; }
+    }
+
+    public class Dependent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int PersonId { get; set; }
+        public required Person Person { get; set; }
+    }
+}
---
+++ PeopleKPMG.Core/Interfaces/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using PeopleKPMG.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleKPMG.Core.Interfaces
+{
+    internal interface Interface1
+    {
+        Task<IEnumerable<Person>> GetAll();
+        Task<Person> GetById(int id);
+        Task Add(Person person);
+        Task Update(Person person);
+        Task Delete(int id);
+    }
+}
