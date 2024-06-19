cd9694e35f6252c3db830d65f7991c60574e7e48
Vitor Lopes <vitor_slopes@hotmail.com>
2024-06-19 00:28:39 -0300

    Adiciona migração e cria de dados + limpeza de código

--- PeopleKPMG.Core/Interfaces/IPersonRepository.cs
+++ PeopleKPMG.Core/Interfaces/IPersonRepository.cs
@@ -1,9 +1,4 @@
 ﻿using PeopleKPMG.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PeopleKPMG.Core.Interfaces
 {
