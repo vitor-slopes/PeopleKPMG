32cf9b53e1109ec6197cd60b34b79cbf73f75734
Vitor Lopes <vitor_slopes@hotmail.com>
2024-06-19 00:24:53 -0300

    Adiciona as camadas API e Infrastructure

--- PeopleKPMG.API/Program.cs
+++ PeopleKPMG.API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PeopleKPMG.Application.Interfaces;
+using PeopleKPMG.Application.Services;
+using PeopleKPMG.Infrastructure.Data;
+using PeopleKPMG.Infrastructure.DependencyInjection;
+using PeopleKPMG.Application.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PeopleContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()));
+
+builder.Services.AddScoped<IPersonService, PersonService>();
+builder.Services.AddInfrastructure();
+builder.Services.AddApplicationServices();
+
 
 var app = builder.Build();
 
---
+++ PeopleKPMG.Infrastructure/Data/PeopleContext.cs
@@ -0,0 +1,12 @@
+﻿using PeopleKPMG.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace PeopleKPMG.Infrastructure.Data
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions<PeopleContext> options) : base(options) { }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Dependent> Dependents { get; set; }
+    }
+}
